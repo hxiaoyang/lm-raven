c8d7c2ea039c8da1413a9b10c004bfbde10c9805
Kyle Barone <kylebarone@kyles-mbp-2.lan>
2023-12-02 17:29:11 -0500

    updates

--- inference.py
+++ inference.py
@@ -230,7 +230,7 @@ class Solver:
         self.context = None
         self.choice_scores = {}
 
-    def __call__(self, config, load_dir, save_dir, b=1, n=3, add_angle=False, subset_dir="subset.json", expieriment_name="", print_prompt=False):
+    def __call__(self, config, load_dir, save_dir, b=1, n=3, add_angle=False, subset_dir="subset.json", experiment_name="", print_prompt=False):
         with open("{}/{}.json".format(load_dir,config), "r") as f:
             samples = json.load(f)
         subset = json.load(open(load_dir+subset_dir, 'r'))
@@ -242,8 +242,8 @@ class Solver:
                 self.output[i] = self._split(sample, config, n=n, add_angle=add_angle, print_prompt=print_prompt)
             else:
                 self.output[i] = self._merge(sample, config, n=n, add_angle=add_angle, print_prompt=print_prompt)
-        if expieriment_name:
-            file_name = f"{len(subset)}_{expieriment_name}.json"
+        if experiment_name:
+            file_name = f"{len(subset)}_{experiment_name}.json"
         else:
             file_name = "{}/{}_500_{}_b{}_n{}.json".format(save_dir, config,
                                                        self.model_name, b, n)
@@ -365,7 +365,7 @@ def main():
     parser.add_argument("--save_dir")
     parser.add_argument("--add_angle", action='store_true')
     parser.add_argument("--subset_dir", default="subset.json")
-    parser.add_argument("--expieriment_name")
+    parser.add_argument("--experiment_name")
     parser.add_argument("--print_prompt", action='store_true')
 
     args = parser.parse_args()
@@ -421,7 +421,7 @@ def main():
         prefix = "let's think step by step. "  # default prefix
     prefix = prefix + "let's think step by step if the following matches \n New Example -> "
     s = Solver(args.model_name, model=model, tokenizer=tokenizer, prefix=prefix)
-    s(args.config, args.load_dir, args.save_dir, b=args.b, n=args.n, add_angle=args.add_angle, subset_dir=args.subset_dir, expieriment_name=args.expieriment_name, print_prompt=args.print_prompt)
+    s(args.config, args.load_dir, args.save_dir, b=args.b, n=args.n, add_angle=args.add_angle, subset_dir=args.subset_dir, experiment_name=args.experiment_name, print_prompt=args.print_prompt)
     return
 
 
